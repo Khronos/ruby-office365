0f4e825f291d69099af236927b9b883aaa6ded92
Martin Little <mlittle@collabushare.com>
2024-09-26 17:55:24 -0400

    Add support for calendarView endpoint for calendar event viewing by date range

--- lib/office365/rest/calendar.rb
+++ lib/office365/rest/calendar.rb
@@ -12,6 +12,13 @@ module Office365
       def calendars(args = {})
         wrap_results(args.merge(kclass: Models::Calendar, base_uri: "/me/calendars"))
       end
+
+      # params: args => { StartDateTime: "<iso8601 DateTime>", EndDateTime: "<iso8601 DateTime>" }
+      def calendar_view(args = {})
+        raise ArgumentError, "StartDateTime and EndDateTime must be provided" unless (args.has_key?(:next_link) && !args[:next_link].blank?) ||
+                                                                                     (args.has_key?(:StartDateTime) && args.has_key?(:EndDateTime))
+        wrap_results(args.merge(kclass: Models::Event, base_uri: ['/me/calendar', 'calendarView'].join("/")))
+      end
     end
   end
 end
